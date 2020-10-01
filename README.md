2377213c4353c2a2d5c3f0932f8ef040eba111d9
asfink <ali@finkelstein.us>
2020-09-30 21:05:59 -0700

    adding service

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-Error [ERR_REQUIRE_ESM]: Must use import to load ES Module: /Users/Ali/Documents/phone-hider/server.js
+# Dear Mom, please stop giving out my phone number 
+
+This is a Vonage based system that handles responding to calls and texts for when my mother gives out my Vonage number without my permission. 
+
+
+## Set up
+### External Dependencies
+
+Make sure you sign up for a [Vonage account](https://www.vonage.com/developer-center/) - you will need to create a new project, and assign a number to the project. 
+
+For local development, make sure you set up [NGROK](https://ngrok.com/) on your system, and run it:
+```bash
+ngrok http 3000
+```
+
+This will enable you to test the endpoints with the Vonage API by exposing the local webserver. 
+
+### ENV Vars 
+
+Create a file `.env` in `/`, and fill in with the relevant information:
+
+```bash
+NEXMO_API_KEY='{API key created for your Vonage Project}'
+NEXMO_API_SECRET='{API secret created for your Vonage project}'
+NEXMO_NUMBER = '{phone number you assign to your Vonage project}'
+```
+
+## Getting Started
+
+Clone the repository, install the packages, and run the development server:
+```bash
+$ npm i
+$ npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+### Caveat
+
+If you receive the following error:
+
+```
+Error [ERR_REQUIRE_ESM]: Must use import to load ES Module
+```
+
+Make sure you're using Node 13.0.0 or higher.
+
+
+## Connecting to Vonage
+
+Once you've deployed the server - either on a service like Heroku or exposed the local endpoints using NGROK - you can then fill out the webhooks on the Vonage website. Each endpoint maps to a specific webhook required by the Vonage API.
+
+For the Voice API - 
+- Answer URL: /webhooks/voice-inbound
+- Event URL: /webhooks/voice-status
+
+For the Messaging API - 
+- Inbound URL: /webhooks/message-inbound
+- Status URL: /webhooks/message-status
+
+
+Specifically, for the Messaging API, you need to also go in and set your "Default SMS Settings" to the same URLs as the ones set for the Messaging API. These settings can be found under `Account>Settings`. 
--- server.js
+++ server.js
@@ -1,20 +1,83 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import Nexmo from 'nexmo';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const nexmo = new Nexmo({
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET,
+})
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res){
-  res.send('Hello World!')
+  res.send('Success')
+})
+
+/**
+ * Handle life cycle webhook calls for the calling events 
+ */
+app.get('/webhooks/voice-status', (req, res) =>{
+    console.log(req.query)
+    res.status(200).end();
+})
+
+/**
+ * Handle inbound calls
+ */
+app.get('/webhooks/voice-inbound', (req,res) =>{
+  const ncco = [{
+    action: 'talk',
+    text: `Thanks for calling Ali.  
+        You received my number via my mom - sadly, 
+        I did not opt into this introduction. 
+        Please ask her to inquire with me first about contacting me. 
+        Thanks for understanding.`
+  }];
+  res.json(ncco)
+})
+
+/**
+ * Handle life cycle webhook calls for the messaging events
+ */
+app.get('/webhooks/message-status', (req, res) =>{
+    console.log(req.query)
+    res.status(200).end();
+})
+
+/**
+ * Handle received texts by sending a response message
+ */
+app.get('/webhooks/message-inbound', (req,res) => {
+    console.log(req.query);
+    const texter = req.query.msisdn;
+    nexmo.message.sendSms(
+        process.env.NEXMO_NUMBER, 
+        texter, 
+        "Hi! Thanks for texting me - sadly, my mom did not ask if I am willing to opt into this introduction. Please ask her to inquire with me first about contacting me. Thanks for understanding.",
+        (err, response) => {
+            if (err){
+                console.log(err)
+                return;
+            }
+            console.log(response.messages[0])
+        }
+    
+    );
+    res.status(200).end();
 })
 
 app.listen(PORT, () => {
   console.log(`Listening on ${ PORT }`);
 });
 
+
+
 process.on('SIGINT', function() {
-    console.log('Quitting...');
-    process.exit(0);
+  console.log('Quitting...');
+  process.exit(0);
 });
